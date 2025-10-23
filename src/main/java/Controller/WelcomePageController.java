9f35f3fa358e4c932ec532aafe97d96f877dbb67
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-23 06:38:03 +0530

    Create WelcomePage

--- src/main/java/Controller/WelcomePageController.java
+++ src/main/java/Controller/WelcomePageController.java
@@ -1,4 +1,23 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WelcomePageController {
+
+    @FXML
+    void btnStart(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/SecurityPage.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
