67bf3496a6d9243d2495f6b05acd665f5547344c
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-23 10:11:11 +0530

    Create login page UI

--- src/main/java/Controller/SignUpPageController.java
+++ src/main/java/Controller/SignUpPageController.java
@@ -2,8 +2,14 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignUpPageController {
 
@@ -17,8 +23,12 @@ public class SignUpPageController {
     private PasswordField txtPassword;
 
     @FXML
-    void btnLogin(ActionEvent event) {
-
+    void btnLogin(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/LoginPage.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
