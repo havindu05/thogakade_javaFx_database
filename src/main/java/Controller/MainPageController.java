06c6c51fb585bab04d860838243af84b19296dbc
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-23 19:11:38 +0530

    Create Mainpage UI

---
+++ src/main/java/Controller/MainPageController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainPageController {
+
+    @FXML
+    void btnCustomerInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHomePage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierInfo(ActionEvent event) {
+
+    }
+
+}
